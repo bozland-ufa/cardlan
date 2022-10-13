629fe2b70bedec587602d05be4123f04aabe3b73
bozland-ufa <bozland@gmail.com>
2022-10-13 10:53:54 +0000

    tes1

--- cardlan/QR/cardlan.c
+++ cardlan/QR/cardlan.c
@@ -4,7 +4,11 @@ void test_cardlan()
 {
 	if(Open_Frambuffer( "/dev/fb0")==MI_OK)
 	{
+		Set_Background(NULL,Color_red,0);
+		usleep(20000);
+
         TextOut(0,10,  "system initialization....",GB2312_16);
+        
     }
 
     Close_Frambuffer();
